fc63e52a846fb9341a37b8a5152196458d0be61c
Akbar Pratama <38770984+AkbarPratama@users.noreply.github.com>
2019-03-06 17:45:52 +0700

    Add files via upload

---
+++ database/migrations/2019_03_06_060321_create_refund_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRefundTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('refund', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('tgl_refund');
+            $table->string('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('refund');
+    }
+}
